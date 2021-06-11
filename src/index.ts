1d369b1a5ecaa067c927c1774deec916491bb722
Jozef Mikušinec <mik.jozef@gmail.com>
2021-06-11 13:33:56 +0200

    Export Token classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Parser } from './parser.js';
-export { Tokenizer } from './tokenizer.js';
+export { Tokenizer, Token, IdentifierToken, NumberToken, TextToken } from './tokenizer.js';
 
 export * from './patterns.js';
