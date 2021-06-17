42a708330aff177833d1cbf267c5791908ba9402
Jozef Mikušinec <mik.jozef@gmail.com>
2021-06-17 13:48:59 +0200

    Export MergedTokens

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Parser } from './parser.js';
+export { Parser, MergedTokens } from './parser.js';
 export { Tokenizer, Token, IdentifierToken, NumberToken, TextToken } from './tokenizer.js';
 
 export * from './patterns.js';
