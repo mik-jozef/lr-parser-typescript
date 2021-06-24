f6ec4ae58a5e2fbe6f0b8519c4a2037f5666bfaf
Jozef Mikušinec <mik.jozef@gmail.com>
2021-06-24 23:23:01 +0200

    Export SrcPosition, bump semver minor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Parser, MergedTokens } from './parser.js';
-export { Tokenizer, Token, IdentifierToken, NumberToken, TextToken } from './tokenizer.js';
+export { Tokenizer, Token, IdentifierToken, NumberToken, TextToken, SrcPosition } from './tokenizer.js';
 
 export * from './patterns.js';
