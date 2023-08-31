37c9bcab919ef6268d9e386b6d51e34dfaa5538e
Jozef Mikušinec <mik.jozef@gmail.com>
2023-08-31 23:03:10 +0200

    Fix a circular dependency bug, bump to v0.4.1
    JS should do better than this

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Parser, MergedTokens } from './parser.js';
 export { Tokenizer, Token, IdentifierToken, NumberToken, TextToken, SrcPosition } from './tokenizer.js';
+export { Parser, MergedTokens } from './parser.js';
 
 export * from './patterns.js';
